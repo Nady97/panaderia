0725b8b108a390c88f0d018e6ca80244c36b084f
nady07 <nadia28.carvajal@gmail.com>
2026-05-08 18:46:42 -0400

    arreglo de tailwindcss..

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite'
 
 
 export default defineConfig({
     plugins: [
+        tailwindcss(),
         laravel({
             // Agrega aquí todos los archivos CSS y JS que quieras compilar
             input: [
