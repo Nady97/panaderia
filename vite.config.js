5ab9255a43001d23a291cd0748b60b2ffc0cb5e3
nady07 <nadia28.carvajal@gmail.com>
2026-05-09 01:50:01 -0400

    Corregir vite.config.js y mover vistas de productos

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,24 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite'
-
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
         tailwindcss(),
         laravel({
-            // Agrega aquí todos los archivos CSS y JS que quieras compilar
             input: [
                 'resources/css/global.css',
+                'resources/css/components.css',
+                'resources/css/sidebar.css',
+                'resources/css/login.css',
                 'resources/js/app.js',
                 'resources/js/perfil.js',
                 'resources/js/usuarios.js',
                 'resources/js/dashboard.js',
                 'resources/js/productos.js',
                 'resources/js/categorias.js',
-                'resources/js/recetas.js',
-
             ],
             refresh: true,
         }),
     ],
-    server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
-        },
-    },
 });
-
-
-
-
