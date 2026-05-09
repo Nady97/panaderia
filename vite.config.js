e8f5215e9157ac5d01302c19fd38d27b3c887984
nady07 <nadia28.carvajal@gmail.com>
2026-05-09 11:50:46 -0400

    style: css.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+
 
 export default defineConfig({
     plugins: [
-        tailwindcss(),
         laravel({
+            // Agrega aquí todos los archivos CSS y JS que quieras compilar
             input: [
                 'resources/css/global.css',
                 'resources/css/components.css',
@@ -17,8 +17,18 @@ export default defineConfig({
                 'resources/js/dashboard.js',
                 'resources/js/productos.js',
                 'resources/js/categorias.js',
+
             ],
             refresh: true,
         }),
     ],
+    server: {
+        watch: {
+            ignored: ['**/storage/framework/views/**'],
+        },
+    },
 });
+
+
+
+
