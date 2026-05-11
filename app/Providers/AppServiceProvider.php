e62d3895a0aa4847aa733d45754a57cfd1371d30
nady07 <nadia28.carvajal@gmail.com>
2026-05-10 20:11:31 -0400

    Fix HTTPS and production config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Gate;
 use App\Models\Usuario;
@@ -22,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+         if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
+        
         Paginator::useBootstrapFive();
 
         // ----------------------------------------------------
